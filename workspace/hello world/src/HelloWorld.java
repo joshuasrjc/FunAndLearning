b919eb9cd55bd600fb16f0723ddce1a24fd08211
fuzzybear3 <stevenguido.guido@gmail.com>
2016-04-19 22:01:53 -0700

    v.2

--- workspace/hello world/src/HelloWorld.java
+++ workspace/hello world/src/HelloWorld.java
@@ -2,7 +2,38 @@
 public class HelloWorld {
 
 	public static void main (String[] args){
+		int x =0; 
 		
-		System.out.println("Hello world");
+		while (x <= 2){
+			
+			System.out.print("Sweet \n");
+			
+			x++;
+			
+		}
+		
+	
+	System.out.println("Hello world");
+		
+	
+		
+	//System.out.println(sum(5,6));
+	
+	System.out.println(multiply((int)Math.pow(1, 21),2));
+	
+	
 	}
+	
+	static int sum(int x, int y){
+		
+		
+		
+		return x+y;
+	}
+	
+	static int multiply(int x, int y){
+		
+		return(x * y);
+	}
+	
 }
