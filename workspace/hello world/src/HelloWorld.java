6e76a5f6297e83af258ecd68f7485f2b44f1bab8
fuzzybear3 <stevenguido.guido@gmail.com>
2016-04-20 19:30:55 -0700

    user input

--- workspace/hello world/src/HelloWorld.java
+++ workspace/hello world/src/HelloWorld.java
@@ -1,46 +1,29 @@
 
+import java.util.Scanner;
 public class HelloWorld {
+
+	private static Scanner input;
 
 	public static void main (String[] args){
 		
-		Player player1 = new Player("steven");
+	input = new Scanner(System.in);
+		 
 		
-		
-
-			player1.x = 5;
-			player1.y = 3;
-			
-			player1.printNameAndPosition();
-		
-		
-		Player player2 = new Player ("niceGuy69");
-		
-			player2.x =5;
-			player2.y = 3;
-			player2.printNameAndPosition();
-			
-		
-		
-		Player player3 = new Player("billy");
-		
-
-			player3.x =5;
-			player3.y = 3;
-			player3.printNameAndPosition();
-			
-		
-	}
 	
-	static int sum(int x, int y){
+		System.out.println("enter you name please: ");
 		
-		
-		
-		return x+y;
-	}
+		String name = input.nextLine();
+
+	//	System.out.println("How are you "+name+"?");
 	
-	static int multiply(int x, int y){
+		System.out.println("well "+name+", go make me dome food!" );
 		
-		return(x * y);
+		
+		
+	
+			
+	
 	}
+
 	
 }
---
+++ workspace/hello world/src/Person.java
@@ -0,0 +1,13 @@
+
+public class Person {
+
+	public static void userName(String name){
+		
+		
+	
+		System.out.println("How are you "+name+"?");
+	
+		System.out.println("well "+name+" go make me dome food!" );
+	
+	}
+}
